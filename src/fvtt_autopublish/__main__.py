2e0ee2d7cd3c25ac58826bf5ab3a880c399d1fc8
Clay Sweetser <Varriount@users.noreply.github.com>
2022-09-03 17:54:09 -0400

    Update __main__.py

--- src/fvtt_autopublish/__main__.py
+++ src/fvtt_autopublish/__main__.py
@@ -1,3 +1,8 @@
+# Click uses argv[0] when printing help messages, etc.
+import sys
+if sys.argv[0] == '__main__.py':
+    sys.argv[0] = 'fvtt-autopublish'
+
 from fvtt_autopublish import main
 
 if __name__ == '__main__':
