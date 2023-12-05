2dccf5307594745faca10ed2f601f1dadfbd1c71
Clay Sweetser <Varriount@users.noreply.github.com>
2023-12-04 20:34:27 -0500

    Make compatible with new package administration UI.

--- src/fvtt_autopublish/__init__.py
+++ src/fvtt_autopublish/__init__.py
@@ -1,70 +1,104 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 import json
 import os
 import re
 import sys
+from dataclasses import dataclass, field
+from enum import Enum
 from getpass import getpass
 from typing import Any, Dict, Optional, Mapping, Sequence
 
 import click
 from click import Choice, Command, BadParameter
 import mechanize
-from mechanize import FormNotFoundError
+from mechanize import FormNotFoundError, HTMLForm, Browser
+
+
+def stderr_print(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
 
 
 # Constants
+PASSWORD_ENV_VARIABLE = "FVTT_PASSWORD"
 MAX_VERSION_COUNT = 1000
 
-ADMIN_URL = 'foundryvtt.com/admin'
-LOGIN_URL = f'https://{ADMIN_URL}/login/'
-MODULE_CONFIG_URL_FMT = f'https://{ADMIN_URL}/packages/package/{{module_id}}/change/'
+ADMIN_URL = "foundryvtt.com"
+LOGIN_URL = f"https://{ADMIN_URL}"
+MODULE_URL_FMT = f"https://{ADMIN_URL}/packages/{{module_id}}/edit"
+LOGIN_COOKIE_NAME = 'sessionid'
 
-PASSWORD_ENV_VARIABLE = "FVTT_PASSWORD"
 
-MANIFEST_KEY_TO_FORM_KEY_MAP = {
-    'version': 'version',
-    # We don't use manifest here, as the manifest URL in the manifest is not
-    # the one we want.
-    'changelog': 'notes',
-    'minimumCoreVersion': 'required_core_version',
-    'compatibleCoreVersion': 'compatible_core_version',
-    'compatibility.minimum': 'required_core_version',
-    'compatibility.verified': 'compatible_core_version',
-}
+class LOGIN_FORM:
+    HTML_ID = "login-form"
+    USERNAME_KEY = "login_username"
+    PASSWORD_KEY = "login_password"
 
-CLI_KEY_TO_FORM_KEY_MAP = {
-    'module_version': 'version',
-    'manifest_url': 'manifest',
-    'changelog': 'notes',
-    'minimum_core_version': ['required_core_version'],
-    'compatible_core_version': ['compatible_core_version'],
-}
+
+class PACKAGE_FORM:
+    CLASS_VALUE = "package-description"
+
+    VERSION_KEY = "version"
+    NOTES_KEY = "notes"
+    MANIFEST_KEY = "manifest"
+    MINIMUM_CORE_VERSION_KEY = "required_core_version"
+    VERIFIED_CORE_VERSION_KEY = "compatible_core_version"
+    MAXIMUM_CORE_VERSION_KEY = "maximum_core_version"
+
+    MANIFEST_KEY_TO_FORM_KEY_MAP = {
+        # We don't include the manifest URL key here, as the manifest URL in the manifest
+        # is a stable link to the latest manifest version, and we want a link to a specific
+        # version's manifest.
+        "version": VERSION_KEY,
+        "changelog": NOTES_KEY,
+        "compatibility.minimum": MINIMUM_CORE_VERSION_KEY,
+        "compatibility.verified": VERIFIED_CORE_VERSION_KEY,
+        "compatibility.maximum": MAXIMUM_CORE_VERSION_KEY,
+        # Deprecated fields
+        "minimumCoreVersion": MINIMUM_CORE_VERSION_KEY,
+        "compatibleCoreVersion": VERIFIED_CORE_VERSION_KEY,
+    }
+
+    CLI_KEY_TO_FORM_KEY_MAP = {
+        "module_version": VERSION_KEY,
+        "changelog": NOTES_KEY,
+        "manifest_url": MANIFEST_KEY,
+        "minimum_core_version": MINIMUM_CORE_VERSION_KEY,
+        "verified_core_version": VERIFIED_CORE_VERSION_KEY,
+        "maximum_core_version": MAXIMUM_CORE_VERSION_KEY,
+        # Deprecated fields
+        "compatible_core_version": VERIFIED_CORE_VERSION_KEY,
+    }
+
 
 # Extend Help Formatting
 class ExtCommand(Command):
-    original_write_dl  = click.formatting.HelpFormatter.write_dl
+    original_write_dl = click.formatting.HelpFormatter.write_dl
     original_wrap_text = click.formatting.wrap_text
 
     def extended_write_dl(self, rows, col_max=10, col_spacing=2):
         return ExtCommand.original_write_dl(rows, col_max, col_spacing)
 
     def extended_wrap_text(
-            self, text, width=78, initial_indent="", subsequent_indent="",
-            preserve_paragraphs=False
+        self,
+        text,
+        width=78,
+        initial_indent="",
+        subsequent_indent="",
+        preserve_paragraphs=False,
     ):
         result = ExtCommand.original_wrap_text(
-            text                = text,
-            width               = width,
-            initial_indent      = initial_indent,
-            subsequent_indent   = subsequent_indent,
-            preserve_paragraphs = preserve_paragraphs
+            text=text,
+            width=width,
+            initial_indent=initial_indent,
+            subsequent_indent=subsequent_indent,
+            preserve_paragraphs=preserve_paragraphs,
         )
 
         result = re.sub(
-            r'\n\n[|]([^|]*?)[|]([^|]*?)[|]',
-            lambda m: '\n' + m[1] + m[2].replace('\n', '\n' + m[1]),
-            result
+            r"\n\n[|]([^|]*?)[|]([^|]*?)[|]",
+            lambda m: "\n" + m[1] + m[2].replace("\n", "\n" + m[1]),
+            result,
         )
         result = result.replace("  [required]", "[required]")
         return result
@@ -81,10 +115,10 @@ class ExtCommand(Command):
     epilog="""
         Utility to publish new module versions to the FoundryVTT module
         administration site.
-    """
+    """,
 )
 @click.option(
-    '--username',
+    "--username",
     help="""
         The username of the account that will be used to access the FoundryVTT
         module administration site.
@@ -96,7 +130,7 @@ class ExtCommand(Command):
     required=True,
 )
 @click.option(
-    '--password-source',
+    "--password-source",
     help="""
         The source that the password of the account that will be used to access
         the site will be read from.
@@ -120,11 +154,11 @@ class ExtCommand(Command):
         |||
     """,
     required=True,
-    type=Choice(['environment', 'input', 'raw-input'], case_sensitive=False),
-    default='input'
+    type=Choice(["environment", "input", "raw-input"], case_sensitive=False),
+    default="input",
 )
 @click.option(
-    '--module-id',
+    "--module-id",
     help="""
         The numeric ID of the module to publish a new version of.
 
@@ -138,7 +172,7 @@ class ExtCommand(Command):
     required=True,
 )
 @click.option(
-    '--manifest-url',
+    "--manifest-url",
     help="""
         The manifest URL of the module version being published.
 
@@ -150,14 +184,14 @@ class ExtCommand(Command):
         |  |The "manifest" URL in the manifest file should be a stable link
         that always points to the *latest* manifest file.|
 
-        |  |The URL used for this option should point to the manifest file
+        |  |The URL used for THIS option should point to the manifest file
         associated with the *specific module version* being published.|
     """,
     metavar="URL",
     default=None,
 )
 @click.option(
-    '--manifest-file',
+    "--manifest-file",
     help="""
         Path of a module manifest file to read information from.
 
@@ -167,11 +201,11 @@ class ExtCommand(Command):
         ||Parameters affected by this option will explicitly state so in their
         description.|
     """,
-    metavar="FILE",
+    metavar="FILE_PATH",
     default=None,
 )
 @click.option(
-    '--module-version',
+    "--module-version",
     help="""
         The new module version to publish.
 
@@ -187,7 +221,7 @@ class ExtCommand(Command):
     default=None,
 )
 @click.option(
-    '--changelog-url',
+    "--changelog-url",
     help="""
         The release notes URL of the module version being published
 
@@ -200,58 +234,73 @@ class ExtCommand(Command):
     default=None,
 )
 @click.option(
-    '--minimum-core-version',
+    "--minimum-core-version",
     help="""
         The oldest version of Foundry required for the module to run properly.
 
         If this option is not used, then a manifest file with a
-        "minimumCoreVersion" key *MUST* be specified via the "--manifest-file"
-        option, in which case the value associated with that key will be used
-        as the value for this option instead.
-    """,
-    metavar="VERSION",
-    default=None,
-)
-@click.option(
-    '--compatible-core-version',
-    help="""
-        The newest version of Foundry that the module will properly run on.
-
-        If this option is not used, then a manifest file with a
-        "compatibleCoreVersion" key *MUST* be specified via the
+        `"compatibility": {"minimum": VERSION}` key *MUST* be specified via the
         "--manifest-file" option, in which case the value associated with that
         key will be used as the value for this option instead.
     """,
     metavar="VERSION",
     default=None,
 )
+@click.option(
+    "--verified-core-version",
+    "--compatible-core-version",
+    help="""
+        The newest version of Foundry that the module will properly run on.
+
+        If this option is not used, then a manifest file with a
+        `"compatibility": {"verified": VERSION}` key *MUST* be specified via the
+        "--manifest-file" option, in which case the value associated with that
+        key will be used as the value for this option instead.
+    """,
+    metavar="VERSION",
+    default=None,
+)
+@click.option(
+    "--maximum-core-version",
+    help="""
+        The newest version of Foundry that the module will properly run on.
+
+        If this option is not used, then a manifest file with a
+        `"compatibility": {"maximum": VERSION}` key *MUST* be specified via the
+        "--manifest-file" option, in which case the value associated with that
+        key will be used as the value for this option instead.
+    """,
+    metavar="VERSION",
+    default=None,
+)
+@click.option("--comptaible-core-version", hidden=True)
 def main(
-        username: str,
-        password_source: str,
-        module_id: int,
-        manifest_file: str,
-        **kwargs
+    username: str,
+    password_source: str,
+    module_id: int,
+    manifest_file: str,
+    **kwargs,
 ):
     new_version_data = {}
 
     # Apply CLI data, if given
-    for cli_key, form_key in CLI_KEY_TO_FORM_KEY_MAP.items():
+    for cli_key, form_key in PACKAGE_FORM.CLI_KEY_TO_FORM_KEY_MAP.items():
         cli_value = kwargs.get(cli_key)
         if cli_value is None:
             continue
 
-        new_version_data[form_key] = kwargs[cli_key]
+        new_version_data[form_key] = cli_value
 
-    # Read in manifest data, if given.
+    # Read in and apply manifest data to new version data, if given.
     if manifest_file is not None:
         manifest_data = read_manifest(manifest_file)
 
         # If present, copy a manifest key to its relevant form key.
-        for manifest_key, form_key in MANIFEST_KEY_TO_FORM_KEY_MAP.items():
+        for manifest_key, form_key in PACKAGE_FORM.MANIFEST_KEY_TO_FORM_KEY_MAP.items():
             if form_key in new_version_data:
                 continue
 
-            manifest_value = get_item_from_path(manifest_data, manifest_key)
+            manifest_value = get_item_from_dotted_path(manifest_data, manifest_key)
             if manifest_value is None:
                 continue
 
@@ -263,93 +312,68 @@ def main(
     except ValueError as exc:
         raise BadParameter("Couldn't read password: " + str(exc))
     if len(password) == 0:
-        print('Warning: Supplied password was empty!', file=sys.stderr)
+        stderr_print("Warning: Supplied password was empty!")
 
     # Now that all the data we require has been gathered, start by initializing
     # the browser object and logging in.
-    br = mechanize.Browser()
+    browser = mechanize.Browser()
 
     # ## Login ## #
-    # Navigate to module administration site's login page.
-    br.open(LOGIN_URL)
+    # Navigate to the login page.
+    browser.open(LOGIN_URL)
 
     # Select and fill out the "login" form.
-    br.select_form(id='login-form')
-    fill_out_login_form(br, username, password)
-    br.submit()
+    browser.select_form(id=LOGIN_FORM.HTML_ID)
+    fill_out_login_form(browser, username, password)
+    browser.submit()
 
     # ## Configuration page ## #
-    # Navigate to configuration page.
-    resp = br.open(MODULE_CONFIG_URL_FMT.format(module_id=module_id))
+    # Navigate to the configuration page.
+    module_config_url = MODULE_URL_FMT.format(module_id=module_id)
+    browser.open(module_config_url)
 
     # Select and fill out the "module versions" form.
     try:
-        br.select_form(id='package_form')
+        browser.select_form(class_=lambda x: PACKAGE_FORM.CLASS_VALUE in x)
     except FormNotFoundError:
-        page_content = br.response().read().decode("utf8")
-        print('Unable to find package configuration form.', file=sys.stderr)
-        print('Are login credentials correct?', file=sys.stderr)
-        print('Page content:', file=sys.stderr)
-        print(re.sub(r'\n{2,}', '\n', page_content))
-    fill_out_version_form(br, new_version_data)
-    br.submit()
-    page_content = br.response().read().decode("utf8")
-    print(re.sub(r'\n{2,}', '\n', page_content))
+        stderr_print("Error encountered!")
+        stderr_print("Debug information:")
+        for key, value in get_debug_browser_information(browser):
+            stderr_print(f"  {key}: {value}")
+        stderr_print()
+        stderr_print("Unable to find package configuration form.")
+        stderr_print(f"Are login credentials correct?")
+        stderr_print(f"The current page URL should be {module_config_url}")
+        return
+    fill_out_version_form(browser, new_version_data)
+    browser.submit()
+
 
 def fill_out_login_form(br, username: str, password: str):
-    br['username'] = username
-    br['password'] = password
+    br[LOGIN_FORM.USERNAME_KEY] = username
+    br[LOGIN_FORM.PASSWORD_KEY] = password
 
 
 def fill_out_version_form(br, new_version_data: Dict):
-    # Get the number of published module versions.
-    # The `versions-TOTAL_FORMS` field_name contains the number of version forms
-    # that are initially on the page.
-    # We assume that the number of version forms is equal to the number of
-    # published versions, plus one (representing the next version).
-    version_count = int(br['versions-TOTAL_FORMS'])
-    current_version_index = version_count - 2
-    new_version_index = version_count - 1
-
-    # The configuration page represents the forms for each version's fields
-    # using the pattern "id_versions-{version_index}-{field_name}".
-    # We define some helpers here for abstracting over this pattern.
-    def versioned_field_name_for(field_name: str, version_index: int):
-        return f'versions-{version_index}-{field_name}'
-
-    # Check the boxes of versions that should be removed.
-    # Note the `range` function, when given a single argument `X`, generates
-    # numbers from 0 to `X - 1`. If `X` is less than or equal to 0, no numbers
-    # will be generated.
-    for version_index in range(version_count - MAX_VERSION_COUNT):
-        # Setting a checkbox control's value automatically "checks" it.
-        versioned_field_name = versioned_field_name_for(
-            field_name='DELETE',
-            version_index=version_index
-        )
-        br[versioned_field_name] = ['on']
-
-    # Conveniently, the page comes with a blank set of form controls that
-    # represent the next version to be published.
-    # All we have to do is fill out the form with data describing the new version.
+    # Fill out the form fields with data describing the new version.
+    form: HTMLForm = br.form
     for field_name, field_value in new_version_data.items():
-        versioned_field_name = versioned_field_name_for(
-            field_name=field_name,
-            version_index=new_version_index
-        )
         # YAML/JSON fields may be numbers, however the form field only accepts strings.
-        br[versioned_field_name] = str(field_value)
+        control = form.find_control(name=field_name)
+        control.disabled = False
+        control.value = str(field_value)
 
 
-def get_item_from_path(container, path):
-    components = path.split('.')
+def get_item_from_dotted_path(container, path):
+    components = path.split(".")
 
     value = container
     for index, component in enumerate(components):
         if isinstance(value, str):
-            partial_path = str.join('.', components[0:index])
+            partial_path = str.join(".", components[0:index])
             raise ValueError(f"Unexpected string at {partial_path} (full path: {path})")
         if isinstance(value, Sequence):
+            partial_path = str.join(".", components[0:index])
             raise ValueError(f"Unexpected list at {partial_path} (full path: {path})")
         else:
             value = value.get(component)
@@ -357,7 +381,7 @@ def get_item_from_path(container, path):
 
 
 def read_manifest(file_path: str) -> Dict[Any, Any]:
-    with open(file_path, 'r') as fh:
+    with open(file_path, "r") as fh:
         return json.load(fh)
 
 
@@ -373,4 +397,27 @@ def read_password(method: str) -> Optional[str]:
         else:
             return result
     else:
-        raise ValueError(f"Invalid method: {method}")
+        raise ValueError(f"Invalid password input method: {method}")
+
+
+def get_debug_browser_information(browser: Browser):
+    yield "Current page URL", browser.geturl()
+    yield "Current page title", browser.title()
+    yield "Currently stored cookies", [cookie.name for cookie in browser.cookiejar]
+    yield "Login cookie is present", LOGIN_COOKIE_NAME in browser.cookiejar
+    # yield "Page content", browser.response()
+
+
+# def get_debug_form_information(form: HTMLForm):
+#     yield "Form name", form.name
+#     yield "Form encoding", form.form_encoding
+#     yield "Form method", form.method
+#     yield "Form action", form.action
+#     yield "Form attrs", form.attrs
+#     response = form.click()
+#     yield response.get_data()
+#     yield response.get_method()
+#     yield response.__getattr__()
+#     yield response.get_type()
+#     yield response.get_full_url()
+#     yield response.get_host()
